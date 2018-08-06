39d140a8a22a9c49db634ad62bc551eb1d74151c
Andrew McOlash <amcolash@gmail.com>
2018-08-05 23:55:18 -0700

    Only show some options on pause menu

--- scenes/ui/menus/options/options_buttons.gd
+++ scenes/ui/menus/options/options_buttons.gd
@@ -5,12 +5,24 @@ func _ready():
     get_state($SFX, "sfx")
     get_state($"V-Sync", "vsync")
 
+    hide_buttons()
+
 func get_state(button, name):
     button.pressed = global_state.options[name]
 
 func set_state(pressed, name):
     global_state.options[name] = pressed
     save_manager.save_data(global_state.options)
+
+func hide_buttons():
+    var menu_only = [
+        $"V-Sync"
+    ]
+
+    var name = get_tree().get_current_scene().get_path().get_name(1)
+    if name.casecmp_to("Options Screen") != 0:
+        for option in menu_only:
+            option.visible = false
 
 func _on_VSync_toggled(button_pressed):
     set_state(button_pressed, "vsync")
